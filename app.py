eba262d960e8b57c9a043c7f4e927a59115949c8
charlottepanck <20167@burnside.school.nz>
2024-05-07 18:09:59 +1200

    testing

--- app.py
+++ app.py
@@ -40,7 +40,57 @@ def bottoms():
     cursor.execute(sql)
     results = cursor.fetchall()
     db.close()
-    return render_template("bottoms.html", results = results)#str(results)
+    return render_template("bottoms.html", results1 = results)#str(results)
+
+
+@app.route('/outerwear')
+def outerwear():
+    db = sqlite3.connect(DB)
+    cursor = db.cursor()
+    sql = """SELECT Outerwear.ID, Outerwear.Name,
+    Brands.Brand_Name, Colours.Colour_Type, Garments.Garment_Type
+    FROM Outerwear
+    LEFT JOIN Brands ON Outerwear.Brand = Brands.ID
+    LEFT JOIN Garments ON Outerwear.Garment = Garments.ID
+    LEFT JOIN Colours ON Outerwear.Colour = Colours.ID;"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    db.close()
+    return render_template("outerwear.html", results2 = results)#str(results)
+
+
+@app.route('/dresses')
+def dresses():
+    db = sqlite3.connect(DB)
+    cursor = db.cursor()
+    sql = """SELECT Dresses.ID, Dresses.Name,
+    Brands.Brand_Name, Colours.Colour_Type, Garments.Garment_Type
+    FROM Dresses
+    LEFT JOIN Brands ON Dresses.Brand = Brands.ID
+    LEFT JOIN Garments ON Dresses.Garment = Garments.ID
+    LEFT JOIN Colours ON Dresses.Colour = Colours.ID;"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    db.close()
+    return render_template("dresses.html", results3 = results)#str(results)
+
+
+@app.route('/outfits')
+def outfits():
+    db = sqlite3.connect(DB)
+    cursor = db.cursor()
+    sql = """SELECT Outfits.ID, Tops.Name, Bottoms.Name, Outerwear.Name, Dresses.Name, Styles.Style_Name
+    FROM Outfits
+    LEFT JOIN Tops ON Outfits.Top == Tops.ID
+    LEFT JOIN Bottoms ON Outfits.Bottoms == Bottoms.ID
+    LEFT JOIN Outerwear ON Outfits.Outerwear == Outerwear.ID
+    LEFT JOIN Dresses ON Outfits.Dress == Dresses.ID
+    LEFT JOIN Styles ON Outfits.Style == Styles.ID;"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    db.close()
+    return render_template("outfits.html", results4 = results)#str(results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
