4891e9c57d60cfba77dc9ef077f3c3b0a0456a49
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-06 14:42:48 +1200

    login system

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def add_user(table_name, add_name, add_password):
     """Add items to the database."""
     with sqlite3.connect(DB) as connection:
         cursor = connection.cursor()
-        sql = f"INSERT INTO {table_name} (username, password) VALUES (?, ?)"
+        sql = f"INSERT INTO {table_name} (username, password) VALUES (?, ?);"
         cursor.execute(sql, (add_name, add_password))
         connection.commit()
 
 
 def search(username, password):
-    """Check if username and password exist in the database."""
+    #Check if username and password exist in the database
     with sqlite3.connect(DB) as connection:
         cursor = connection.cursor()
         sql = "SELECT * FROM user WHERE username = ?"
@@ -40,20 +40,22 @@ def search(username, password):
             print("User doesn't exist")
             return False, None
 
-@app.route("/user")
-def user():
-    return render_template('user.html')
 
-@app.route("/user/<int:user_id>")
+@app.route("/index/<int:user_id>")
 def get_user_id(user_id):
     if "user_id" in session and session["user_id"] == user_id:
         with sqlite3.connect(DB) as connection:
             cursor = connection.cursor()
             cursor.execute("SELECT * FROM user WHERE user_id = ?;", (user_id,))
-            user = cursor.fetchone()
-        return render_template("user.html", user = user)
+            user = cursor.fetchall()
+        return render_template("index.html", user = user)
     else:
         return redirect(url_for("login"))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 @app.route("/signup")
@@ -81,12 +83,12 @@ def login():
         return render_template("login.html")
 
 
-#@app.route("/logout")
-#def logout():
-#    session.pop("user_id", None)
-#    return redirect(url_for("login"))
+@app.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect(url_for("login"))
 
-
+            
 @app.route("/add_user")
 def add_user_route():
     username = request.args.get('username')
@@ -94,127 +96,137 @@ def add_user_route():
     confirm_password = request.args.get('confirm')
     with sqlite3.connect(DB) as connection:
         cursor = connection.cursor()
-        sql = "SELECT * FROM user WHERE username = ?;"
+        sql = "SELECT * FROM user WHERE username = ?"
         cursor.execute(sql, (username,))
         user = cursor.fetchone()
         if user and username == user[1]:
             error_message = "Username is taken, please use another name"
-            return render_template("signup.html", error_message=error_message)
+            return render_template("signup.html", title="Sign up", error_message=error_message)
         if password == confirm_password:
-            cursor.execute("INSERT INTO user (username, password) VALUES (?, ?);", (username, password))
+            cursor.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, password))
             connection.commit()
-            return render_template("user.html")
+            return render_template("login.html")
         else:
-            error_message = "Your confirmed password is incorrect, please try agaisn"
-            return render_template("signup.html", error_message=error_message)
+            error_message = "Your confirmed password is incorrect, please try again"
+            return render_template("signup.html", title="Sign up", error_message=error_message)
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 # view clothing
-@app.route('/clothing')
-def viewclothing():
-    # query to find all tops and filter by catagories
-    # render poduct page
-    db = sqlite3.connect(DB)
-    cursor = db.cursor()
-    sql = """SELECT clothing.clothing_id, clothing.name, brand.brand_name, type.type_name, colour.colour_name, clothing.img_file 
+@app.route('/clothing/<int:user_id>')
+def viewclothing(user_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        db = sqlite3.connect(DB)
+        cursor = db.cursor()
+        cursor.execute("""SELECT clothing.clothing_id, clothing.name, brand.brand_name, type.type_name, colour.colour_name, clothing.img_file, clothing.user_id 
 FROM clothing 
 LEFT JOIN brand on clothing.brand = brand.brand_id
 LEFT JOIN type on clothing.type = type.type_id
 LEFT JOIN colour on clothing.colour = colour.colour_id
-ORDER BY clothing.name;"""
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    sql2 = "SELECT * FROM brand;"
-    cursor.execute(sql2)
-    resultsbrandsc = cursor.fetchall()
-    sql3 = "SELECT * FROM colour;"
-    cursor.execute(sql3)
-    resultscoloursc = cursor.fetchall()
-    sql4 = "SELECT * FROM type;"
-    cursor.execute(sql4)
-    resultstypec = cursor.fetchall()
-    db.close()
-    return render_template("clothing.html", results=results, resultsbrandsc=resultsbrandsc, resultstypec=resultstypec, resultscoloursc=resultscoloursc)
+WHERE clothing.user_id = ?;""", (user_id,))
+        results = cursor.fetchall()
+
+        #sql2 = "SELECT * FROM brand WHERE user_id =?;"
+        cursor.execute("SELECT * FROM brand WHERE user_id =?", (user_id,))
+        resultsbrandsc = cursor.fetchall()
+
+        #sql3 = "SELECT * FROM colour WHERE user_id =?;"
+        cursor.execute("SELECT * FROM colour WHERE user_id =?", (user_id,))
+        resultscoloursc = cursor.fetchall()
+
+        #sql4 = "SELECT * FROM type WHERE user_id =?;"
+        cursor.execute("SELECT * FROM type WHERE user_id =?", (user_id,))
+        resultstypec = cursor.fetchall()
+
+        db.close()
+        return render_template("clothing.html", user_id=user_id, results=results, resultsbrandsc=resultsbrandsc, resultstypec=resultstypec, resultscoloursc=resultscoloursc)
+    else:
+        return redirect(url_for('login'))
 
 
 # view catagories
-@app.route('/catagories')
-def viewcatagories():
-    db = sqlite3.connect(DB)
-    cursor = db.cursor()
-    sql = "SELECT * FROM colour;"
-    cursor.execute(sql)
-    colour_results = cursor.fetchall()
-    sql2 = "SELECT * FROM type;"
-    cursor.execute(sql2)
-    type_results = cursor.fetchall()
-    sql3 = "SELECT * FROM brand;"
-    cursor.execute(sql3)
-    brands_results = cursor.fetchall()
-    db.close()
-    return render_template("catagories.html", colour_results=colour_results, type_results=type_results, brands_results=brands_results)
-
+@app.route('/catagories/<int:user_id>')
+def viewcatagories(user_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        db = sqlite3.connect(DB)
+        cursor = db.cursor()
+        #sql = "SELECT * FROM colour WHERE user_id=?;"
+        cursor.execute("SELECT * FROM colour WHERE user_id=?;",(user_id,))
+        colour_results = cursor.fetchall()
+        #sql2 = "SELECT * FROM type;"
+        cursor.execute("SELECT * FROM type WHERE user_id=?",(user_id,))
+        type_results = cursor.fetchall()
+        #sql3 = "SELECT * FROM brand;"
+        cursor.execute("SELECT * FROM brand;",(user_id,))
+        brands_results = cursor.fetchall()
+        db.close()
+        return render_template("catagories.html", user_id=user_id, colour_results=colour_results, type_results=type_results, brands_results=brands_results)
+    else:
+        return redirect(url_for('login'))
 
 # view outfits
 @app.route('/outfits')
 def viewoutfits():
-    db = sqlite3.connect(DB)
-    cursor = db.cursor()
-    sql = """SELECT outfit.id, outfit.outfit_id, clothing.name, style.style_name, clothing.img_file
-FROM outfit 
-LEFT JOIN style ON outfit.outfit_style = style.style_id
-LEFT JOIN clothing ON outfit.outfit_img_file = clothing.clothing_id;"""
-    cursor.execute(sql)
-    outfits_results = cursor.fetchall()
-    sql1 = "SELECT clothing_id, name FROM clothing;"
-    cursor.execute(sql1)
-    resultsclothingo = cursor.fetchall()
-    sql2 = "SELECT * FROM style;"
-    cursor.execute(sql2)
-    resultsstyleo = cursor.fetchall()
-    sql3 = "SELECT clothing_id, img_file, name FROM clothing;"
-    cursor.execute(sql3)
-    resultsimg_fileo = cursor.fetchall()
-    db.close()
-    return render_template("outfits.html", outfits_results = outfits_results, resultsstyleo=resultsstyleo, resultsclothingo=resultsclothingo, resultsimg_fileo=resultsimg_fileo)
+    if 'user_id' in session:
+        user_id = session['user_id']
+        db = sqlite3.connect(DB)
+        cursor = db.cursor()
+        cursor.execute("""SELECT outfit.id, outfit.outfit_id, clothing.name, style.style_name, clothing.img_file, outfit.user_id
+    FROM outfit 
+    LEFT JOIN style ON outfit.outfit_style = style.style_id
+    LEFT JOIN clothing ON outfit.outfit_img_file = clothing.clothing_id
+    WHERE outfit.user_id=?;""", (user_id,))
+        outfits_results = cursor.fetchall()
+
+        #sql1 = "SELECT clothing_id, name FROM clothing WHERE user_id=?;"
+        cursor.execute("SELECT clothing_id, name FROM clothing WHERE user_id=?;", (user_id,))
+        resultsclothingo = cursor.fetchall()
+        #sql2 = "SELECT * FROM style WHERE user_id=?;"
+        cursor.execute("SELECT * FROM style WHERE user_id=?;", (user_id,))
+        resultsstyleo = cursor.fetchall()
+        #sql3 = "SELECT clothing_id, img_file, name FROM clothing WHERE user_id=?;"
+        cursor.execute("SELECT clothing_id, img_file, name FROM clothing WHERE user_id=?;", (user_id,))
+        resultsimg_fileo = cursor.fetchall()
+        db.close()
+        return render_template("outfits.html", user_id=user_id, outfits_results = outfits_results, resultsstyleo=resultsstyleo, resultsclothingo=resultsclothingo, resultsimg_fileo=resultsimg_fileo)
 
 
 # add clothing
 @app.route('/clothing', methods=['GET', 'POST'])
 def addclothing():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        brand = request.form.get('brand')
-        type = request.form.get('type')
-        colour = request.form.get('colour')
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-    
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-    
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            name = request.form.get('name')
+            brand = request.form.get('brand')
+            type = request.form.get('type')
+            colour = request.form.get('colour')
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
         
-        with sqlite3.connect(DB) as connection:
-            cursor = connection.cursor()
-            sql = "INSERT INTO clothing (name, brand, type, colour, img_file) VALUES (?, ?, ?, ?, ?);"
-            cursor.execute(sql, (name, brand, type, colour, filename))
-            connection.commit()
-            return viewclothing()
+            file = request.files['file']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+        
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            with sqlite3.connect(DB) as connection:
+                cursor = connection.cursor()
+                sql = "INSERT INTO clothing (name, brand, type, colour, img_file, user_id) VALUES (?, ?, ?, ?, ?, ?);"
+                cursor.execute(sql, (name, brand, type, colour, filename, user_id))
+                connection.commit()
+            return redirect(url_for('get_user_id', user_id=user_id))
+        return viewclothing(user_id)
     else:
-        return viewclothing()
-    
+        return redirect(url_for('login'))
 
-# delete clothing
+
+#delete clothing
 @app.route('/delete_clothing/<int:ID>', methods=['POST'])
 def deleteclothing(ID):
     with sqlite3.connect(DB) as connection:
@@ -227,63 +239,93 @@ def deleteclothing(ID):
 
 # add outfit
 @app.route('/outfit', methods=['GET', 'POST'])
-def addoutfit():
-    if request.method == 'POST':
-        outfit_id = request.form.get('outfit_id',)
-        outfit_style = request.form.get('outfit_style')
-        outfit_img_file = request.form.get('outfit_img_file')
-        
-        with sqlite3.connect(DB) as connection:
-            cursor = connection.cursor()
-            sql = "INSERT INTO outfit (outfit_id, outfit_style, outfit_img_file) VALUES (?, ?, ?);"
-            cursor.execute(sql, (outfit_id, outfit_style, outfit_img_file))
-            connection.commit()
-            return viewoutfits()
+def addoutfit(user_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            outfit_id = request.form.get('outfit_id',)
+            outfit_style = request.form.get('outfit_style')
+            outfit_img_file = request.form.get('outfit_img_file')
+            
+            with sqlite3.connect(DB) as connection:
+                cursor = connection.cursor()
+                sql = "INSERT INTO outfit (outfit_id, outfit_style, outfit_img_file, user_id) VALUES (?, ?, ?, ?);"
+                cursor.execute(sql, (outfit_id, outfit_style, outfit_img_file, user_id))
+                connection.commit()
+            return redirect(url_for('get_user_id', user_id=user_id))
+        return viewoutfits(user_id)
     else:
-        return viewoutfits()
-    
+        return redirect(url_for('login'))
 
 # add brand
 @app.route('/add_brand', methods=['GET', 'POST'])
-def add_brand():
-    if request.method == 'POST':
-        brand_name = request.form.get('brand_name')
+def add_brand(user_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            brand_name = request.form.get('brand_name')
 
-        with sqlite3.connect(DB) as connection:
-            cursor = connection.cursor()
-            sql = "INSERT INTO brand (brand_name) VALUES (?);"
-            cursor.execute(sql, (brand_name,))
-            connection.commit()
-            return viewcatagories()
+            with sqlite3.connect(DB) as connection:
+                cursor = connection.cursor()
+                sql = "INSERT INTO brand (brand_name, user_id) VALUES (?, ?);"
+                cursor.execute(sql, (brand_name, user_id))
+                connection.commit()
+            return redirect(url_for('get_user_id', user_id=user_id))
+        return viewcatagories(user_id)
     else:
-        return viewcatagories()
-
+        return redirect(url_for('login'))
 
 # add coloue
 @app.route('/add_colour', methods=['GET', 'POST'])
-def add_colour():
-    if request.method == 'POST':
-        colour_name = request.form.get('colour_name')
+def add_colour(user_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            colour_name = request.form.get('colour_name')
 
-        with sqlite3.connect(DB) as connection:
-            cursor = connection.cursor()
-            sql = "INSERT INTO colour (colour_name) VALUES (?);"
-            cursor.execute(sql, (colour_name,))
-            connection.commit()
-            return viewcatagories()
+            with sqlite3.connect(DB) as connection:
+                cursor = connection.cursor()
+                sql = "INSERT INTO colour (colour_name, user_id) VALUES (?, ?);"
+                cursor.execute(sql, (colour_name, user_id))
+                connection.commit()
+            return redirect(url_for('get_user_id', user_id=user_id))
+        return viewcatagories(user_id)
     else:
-        return viewcatagories()
+        return redirect(url_for('login'))
+
+
+# add type
+@app.route('/add_type', methods=['GET', 'POST'])
+def add_type(user_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            type_name = request.form.get('type_name')
+
+            with sqlite3.connect(DB) as connection:
+                cursor = connection.cursor()
+                sql = "INSERT INTO type (type_name, user_id) VALUES (?, ?);"
+                cursor.execute(sql, (type_name, user_id))
+                connection.commit()
+            return viewcatagories(user_id)
+        return viewcatagories(user_id)
+    else:
+        return redirect(url_for('login'))
 
 
 # delete colour
 @app.route("/delete_colour/<int:ID>", methods=["POST"])
 def delete_colour(ID):
-    with sqlite3.connect(DB) as connection:
-        cursor = connection.cursor()
-        sql_delete = "DELETE FROM colour WHERE colour_id = ?;"
-        cursor.execute(sql_delete, (ID,))
-        connection.commit()
-    return viewcatagories()
+    if 'user_id' in session:
+        user_id = session['user_id']
+        with sqlite3.connect(DB) as connection:
+            cursor = connection.cursor()
+            sql_delete = "DELETE FROM colour WHERE colour_id = ?;"
+            cursor.execute(sql_delete, (ID,))
+            connection.commit()
+        return viewcatagories(user_id)
+    else:
+        return redirect(url_for('login'))
 
 # delete type
 @app.route("/delete_type/<int:ID>", methods=["POST"])
@@ -304,22 +346,6 @@ def delete_brand(ID):
         cursor.execute(sql_delete, (ID,))
         connection.commit()
     return viewcatagories()
-
-# add type
-@app.route('/add_type', methods=['GET', 'POST'])
-def add_type():
-    if request.method == 'POST':
-        type_name = request.form.get('type_name')
-
-        with sqlite3.connect(DB) as connection:
-            cursor = connection.cursor()
-            sql = "INSERT INTO type (type_name) VALUES (?);"
-            cursor.execute(sql, (type_name,))
-            connection.commit()
-            return viewcatagories()
-    else:
-        return viewcatagories()
-
 
 # delete outfit
 @app.route('/delete_outfit/<int:ID>', methods=['POST'])
